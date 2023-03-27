bac88a3ef2def07655c76b0be2755bd827d5a31d
naztekinalp <naz.tekinalp2@hotmail.com>
2023-03-27 12:42:49 +0300

    - Change if condition for null check

--- HW2/LengthOfLastWord.java
+++ HW2/LengthOfLastWord.java
@@ -4,13 +4,19 @@ public class LengthOfLastWord {
     public static void main(String[] args) {
         System.out.println(lengthOfLastWord(" fly me to the moon "));
     }
-        public static int lengthOfLastWord(String s) {
-            String trimmedString = s.trim();
-            if (trimmedString == null || trimmedString.length() == 0) {
-                return 0;
-            } else {
-                String lastWord = trimmedString.substring(trimmedString.lastIndexOf(" ") + 1);
-                return lastWord.length();
-            }
+
+    public static int lengthOfLastWord(String s) {
+        if (s == null) {
+            return 0;
+        }
+
+        String trimmedString = s.trim();
+
+        if (trimmedString.length() == 0) {
+            return 0;
+        } else {
+            String lastWord = trimmedString.substring(trimmedString.lastIndexOf(" ") + 1);
+            return lastWord.length();
+        }
     }
 }
