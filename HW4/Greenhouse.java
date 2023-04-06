3b2e4fd11797e297c069d5dd93a6a7b3f5e841d0
naztekinalp <naz.tekinalp2@hotmail.com>
2023-04-06 13:31:01 +0300

    Change update methods

--- HW4/Greenhouse.java
+++ HW4/Greenhouse.java
@@ -9,25 +9,9 @@ public class Greenhouse {
 
     List<Plant> plants = new ArrayList<Plant>();
 
-    void buyNewPlant(String plant) {
-
-        if (!plant.equals("bonsai") && !plant.equals("raspberry") && !plant.equals("rose")) {
-            System.out.println("Invalid plant");
-        } else if (plant.equals("bonsai")) {
-            plants.add(new Bonsai(plants.size()));
-            this.balance = this.balance - Bonsai.price;
-            System.out.println("Bought bonsai");
-        } else if (plant.equals("raspberry")) {
-            plants.add(new Raspberry(plants.size()));
-            this.balance = this.balance - Raspberry.price;
-            System.out.println("Bought raspberry");
-
-        } else {
-            plants.add(new Rose(plants.size()));
-            this.balance = this.balance - Rose.price;
-            System.out.println("Bought rose");
-
-        }
+    void buyNewPlant(Plant plant) {
+        this.plants.add(plant);
+        System.out.println("Plant is bought");
     }
 
     void waterPlants() {
@@ -39,7 +23,7 @@ public class Greenhouse {
 
     void changeTemperature(int temperature) {
         this.temperature = temperature;
-        System.out.println("Temperature changed to " +temperature);
+        System.out.println("Temperature changed to " + temperature);
     }
 
     void removeAPlant(int id) {
--- HW4/Main.java
+++ HW4/Main.java
@@ -4,9 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Greenhouse greenhouse = new Greenhouse();
 
-        greenhouse.buyNewPlant("bonsai");
-        greenhouse.buyNewPlant("raspberry");
-        greenhouse.buyNewPlant("rose");
+        Bonsai bonsai = new Bonsai(1);
+        bonsai.updateNationRegion("China");
+
+        Raspberry raspberry = new Raspberry(2);
+        raspberry.updateNationRegion("Canada");
+
+        Rose rose = new Rose(3);
+        rose.updateType("Red rose");
+
+        greenhouse.buyNewPlant(bonsai);
+        greenhouse.buyNewPlant(raspberry);
+        greenhouse.buyNewPlant(rose);
 
         greenhouse.waterPlants();
 
@@ -15,6 +24,7 @@ public class Main {
         greenhouse.removeAPlant(1);
 
         greenhouse.retrievePlantsFromJP();
+
 
     }
 }
