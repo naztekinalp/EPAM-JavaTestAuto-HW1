74255736e50530878ae0233285f01c369009ca4f
naztekinalp <naz.tekinalp2@hotmail.com>
2023-04-21 14:55:14 +0300

    Reformat Task3

--- HW7/TwoSets.java
+++ HW7/TwoSets.java
@@ -6,17 +6,19 @@ import java.util.*;
 public class TwoSets {
 
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        Set<Integer> set2 = new HashSet<Integer>(Arrays.asList(4, 5, 6, 7, 8));
 
-        System.out.println("Set 1: " + set1);
-        System.out.println("Set 2: " + set2);
+        Set<Integer> set1 = new HashSet<Integer>(Arrays.asList(0, 1, 3, 5, 7));
+        Set<Integer> set2 = new HashSet<Integer>(Arrays.asList(2, 4, 5, 7, 8));
+
+        System.out.println("Set 1 is: " + set1);
+        System.out.println("Set 2 is: " + set2);
+        System.out.println();
 
         Set<Integer> unionSet = union(set1, set2);
-        System.out.println("Union of Set 1 and Set 2: " + unionSet);
+        System.out.println("Union of the Set 1 and Set 2: " + unionSet);
 
         Set<Integer> intersectionSet = intersection(set1, set2);
-        System.out.println("Intersection of Set 1 and Set 2: " + intersectionSet);
+        System.out.println("Intersection of the Set 1 and Set 2: " + intersectionSet);
     }
 
     public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
