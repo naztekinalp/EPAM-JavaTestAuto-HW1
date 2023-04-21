26a9511891f72dc8f4113cfe6f8c42557292a254
naztekinalp <naz.tekinalp2@hotmail.com>
2023-04-21 15:31:35 +0300

    Update WordCount.java

--- HW7/WordCount.java
+++ HW7/WordCount.java
@@ -1,41 +1,39 @@
 package HW7;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 //TASK 5
 public class WordCount {
+
     public static void main(String[] args) {
-        String fileName = "input_task1.txt"; // replace with the name of your text file
-        HashMap<String, Integer> wordCounts = new HashMap<>();
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        String filePath = "./HW7/input_task5.txt";
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line = reader.readLine();
+            Scanner scanner = new Scanner(new File(filePath));
 
-            while (line != null) {
-                String[] words = line.split(" ");
-                for (String word : words) {
-                    if (!word.isEmpty()) {
-                        word = word.toLowerCase(); // convert to lowercase to ignore case sensitivity
-                        if (wordCounts.containsKey(word)) {
-                            wordCounts.put(word, wordCounts.get(word) + 1);
-                        } else {
-                            wordCounts.put(word, 1);
-                        }
-                    }
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+
+                if (wordCounts.containsKey(word)) {
+                    int count = wordCounts.get(word);
+                    wordCounts.put(word, count + 1);
+                } else {
+
+                    wordCounts.put(word, 1);
                 }
-                line = reader.readLine();
             }
-            reader.close();
-        } catch (Exception e) {
-            System.err.format("Exception occurred trying to read '%s'.", fileName);
-            e.printStackTrace();
+
+            for (String word : wordCounts.keySet()) {
+                int count = wordCounts.get(word);
+                System.out.println(word + ": " + count);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
         }
-
-        System.out.println(wordCounts); // print the word counts to the console
-
-        // you can save the word counts to a file or do further processing here
     }
 }
