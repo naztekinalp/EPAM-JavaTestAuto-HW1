2eadddd10698f586d73cc46ca289d965c3640a51
naztekinalp <naz.tekinalp2@hotmail.com>
2023-04-21 15:23:34 +0300

    Update UniqueWords.java

--- HW7/UniqueWords.java
+++ HW7/UniqueWords.java
@@ -1,42 +1,33 @@
 package HW7;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 //TASK 4
 public class UniqueWords {
     public static void main(String[] args) {
-        // Change the file path to the location of your text file
-        File file = new File("path/to/your/textfile.txt");
 
-        // Create a HashSet to store the unique words
+        File file = new File("./HW7/input_task4.txt");
+
         Set<String> uniqueWords = new HashSet<>();
 
         try {
-            // Use a Scanner to read the text file line by line
             Scanner scanner = new Scanner(file);
 
             while (scanner.hasNextLine()) {
-                // Read each line of the file and split it into words
+
                 String[] words = scanner.nextLine().split("\\s+");
 
-                // Add each word to the HashSet
                 for (String word : words) {
                     uniqueWords.add(word);
                 }
             }
 
-            // Close the scanner
-            scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        // Print out the unique words
-        System.out.println("Unique words in the file:");
+        System.out.println("Unique words in the txt:");
         for (String word : uniqueWords) {
             System.out.println(word);
         }
