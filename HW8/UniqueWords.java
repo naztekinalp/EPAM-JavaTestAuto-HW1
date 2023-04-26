e7da339d3efcd397d87bb6ae43e5fd15c11bd999
naztekinalp <naz.tekinalp2@hotmail.com>
2023-04-26 23:30:11 +0300

    Update task 1

---
+++ HW8/UniqueWords.java
@@ -0,0 +1,35 @@
+//TASK 4
+package HW8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UniqueWords {
+    public static void main(String[] args) {
+
+        File file = new File("./HW7/input_task4.txt");
+
+        Set<String> uniqueWords = new HashSet<>();
+
+        try (Scanner scanner = new Scanner(file)) {
+            scanner.useDelimiter("\\W+");
+            uniqueWords = scanner.tokens()
+                    .map(String::toLowerCase)
+                    .distinct()
+                    .collect(Collectors.toSet());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Unique words in the txt:");
+        uniqueWords.forEach(System.out::println);
+    }
+}
+
+
+
