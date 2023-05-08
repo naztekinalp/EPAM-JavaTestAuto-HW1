aab7ac2e8d700a43a3624c3b57a5af45cb6ca18d
naztekinalp <naz.tekinalp2@hotmail.com>
2023-05-08 10:55:15 +0300

    Implement HW10

---
+++ HW10/Gardener.java
@@ -0,0 +1,11 @@
+package HW10;
+
+public class Gardener {
+    private int gardenerId;
+    private String name;
+    private String phone;
+    private String email;
+    private int greenhouseId;
+
+    // Constructor, getters, and setters
+}
---
+++ HW10/Greenhouse.java
@@ -0,0 +1,11 @@
+package HW10;
+
+public class Greenhouse {
+    private int greenhouseId;
+    private String name;
+    private String location;
+    private int capacity;
+    private int gardenerId;
+
+    // Constructor, getters, and setters
+}
